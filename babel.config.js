05d784b25a8bace53ad984fe5772cc062634fe2e
Paulo Dantas <paulo.roberto@hdn.digital>
2022-06-03 13:38:58 -0300

    Initial Project

---
+++ babel.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@config': './src/config',
+          '@utils': './src/utils',
+          '@models': './src/models',
+          '@controllers': './src/controllers',
+          '@views': './src/views',
+        },
+      },
+    ],
+  ],
+  ignore: ['**/*.spec.ts'],
+};
